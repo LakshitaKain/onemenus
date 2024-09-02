3d002ae9fa7ff7d55b8228a2ba56d18b5d3ebd74
kaushik5309 <kaushik.mandal@collegedunia.com>
2024-09-02 18:47:05 +0530

    Update api_L1.py

--- L1_classifier/api_L1.py
+++ L1_classifier/api_L1.py
@@ -15,17 +15,6 @@ router_l1 = APIRouter()
 config = load_yaml_config("/root/ocr/CLIP/config.yaml")['L1']  # Change 'L1' to 'L2' as needed
 clip_infer = CLIPInfer(config)
 
-# @router_l1.post("/predict")
-# async def predict_label(image_url):
-#     try:
-#         predicted_label = clip_infer.predict_label(image_url)
-#         if predicted_label is not None:
-#             return {"predicted_label": predicted_label}
-#         else:
-#             raise HTTPException(status_code=400, detail="Prediction failed.")
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
 
 from pydantic import BaseModel
 
