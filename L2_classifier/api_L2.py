56c24a8f53873457f75390a22d08386dabe52abf
rs7520 <riya.soni@collegedunia.com>
2024-09-04 16:18:06 +0530

    Update api_L2.py

--- L2_classifier/api_L2.py
+++ L2_classifier/api_L2.py
@@ -1,44 +1,23 @@
-import yaml
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from clip_pipeline_2 import CLIPInfer
 
-# Load configuration from YAML file
-def load_yaml_config(file_path):
-    with open(file_path, 'r') as file:
-        return yaml.safe_load(file)
-
 # Create an instance of the APIRouter
 router_l2 = APIRouter()
 
-# Load configuration
-config = load_yaml_config("/root/l2_classifier/config/congi.yaml")['L2']  # Change 'L1' to 'L2' as needed
-clip_infer = CLIPInfer(config)
-
-# @router_l1.post("/predict")
-# async def predict_label(image_url):
-#     try:
-#         predicted_label = clip_infer.predict_label(image_url)
-#         if predicted_label is not None:
-#             return {"predicted_label": predicted_label}
-#         else:
-#             raise HTTPException(status_code=400, detail="Prediction failed.")
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-from pydantic import BaseModel
+# Initialize the CLIPInfer class
+clip_infer = CLIPInfer()
 
 class PredictRequest(BaseModel):
-   image_url: str
+    image_url: str
 
-@router_l2.post("/predict")
+@router_l2.post("/predict_L2")
 async def predict_label(request: PredictRequest):
-   try:
-       predicted_label = clip_infer.predict_label(request.image_url)
-       if predicted_label is not None:
-           return int(predicted_label)
-       else:
-           raise HTTPException(status_code=400, detail="Prediction failed.")
-   except Exception as e:
-       raise HTTPException(status_code=500, detail=str(e))
+    try:
+        predicted_label = clip_infer.predict_label(request.image_url)
+        if predicted_label is not None:
+            return {"predicted_label": int(predicted_label)}
+        else:
+            return {"detail": "Prediction failed."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
