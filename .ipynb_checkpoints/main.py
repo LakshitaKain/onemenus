0bbac978c377004aa52cf1a4a183a32e4cd143a3
root <root@e2e-87-202.ssdcloudindia.net>
2024-11-08 18:52:23 +0530

    removed ipynb checkpoints

--- .ipynb_checkpoints/main.py
+++
@@ -1,7 +0,0 @@
-from menu_data_fetch.main import main
-from clip_l1.main import predict
-from clip_l2.main import predict_label
-from yolo_robo.main import yolo_bg
-from orientation.main import process_image
-from cluster.main import generate_best_images_from_google_id
-
