3e5c03dfd352fd807db75fdbe7f8e8da577583a0
Ronit Akhariya <ronitak33@gmail.com>
2024-11-08 18:40:57 +0530

    optimized sunny's code for yolo segment creation

--- algo_production/yolo/inference_new.py
+++ algo_production/yolo/inference_new.py
@@ -3,46 +3,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
-model = YOLO(r"C:/Users/sunny/new_pro/last_production/yolo/models/best.pt")
-
-
-def yolo_segment(image: np.ndarray):
-
+model = YOLO("best.pt")
+def yolo_segment(image: np.ndarray, l1_label):
     try:
         results = model.predict(image)
         segments = []
-
+        width, height, _ = image.shape
         for result in results:
             boxes = result.boxes  # Bounding boxes
             xyxy = boxes.xyxy.cpu().numpy()  # Coordinates in (x1, y1, x2, y2) format
+            xywh = boxes.xywh.cpu().numpy()
+            confs = boxes.conf
+            
             names = result.names
             cls = boxes.cls.cpu().numpy()  # Class indices
+            
+            if l1_label in [0, 1]:
+                for i, box in enumerate(xyxy):
+                    # try:
+                        x1, y1, x2, y2 = map(int, box)
+                        segment = image[y1:y2, x1:x2]
+                        cv2.imwrite(f"segs/seg{i}.jpg", segment)
+                        segments.append(segment)  # Append each cropped segment as np.ndarray
+                
+            else:
+                bounding_threshold = 0.50
+                edge_threshold = 3
+                
+                for i, (box, conf) in enumerate(zip(xywh, confs)):
+                    try:
+                        if conf > bounding_threshold:
+                            x, y, w, h = box
+                            if x <= edge_threshold or x + w >= (width - edge_threshold):
+                                pass
 
-            for i, box in enumerate(xyxy):
-                try:
-                    x1, y1, x2, y2 = map(int, box)
-                    
-                    segment = image[y1:y2, x1:x2]  
-                    segments.append(segment)  # Append each cropped segment as np.ndarray
-
-                    # # Display each segment
-                    # plt.imshow(segment)
-                    # plt.title(f"Segment {i} - Class: {names[int(cls[i])]}")
-                    # plt.axis("off")
-                    # plt.show()
-                    
-                except Exception as e:
-                    print(f"Error processing segment {i}: {e}")
-                    continue  # Move to the next segment if an error occurs
+                            elif y <= edge_threshold or y + h >= (height - edge_threshold):
+                                pass
+                            else:
+                                x1 = int(x)
+                                y1 = int(y)
+                                x2 = int(x+w)
+                                y2 = int(y+h)
+                                segment = image[y1:y2, x1:x2]
+                                cv2.imwrite(f"seg{i}.jpg", segment)
+                                segments.append(segment)
+                    except Exception as e:
+                        print(f"Error processing class 2: {e}")
+                        
         return segments
-
-    except Exception as e:
-        print(f"Error during YOLO segmentation: {e}")
-        return []
     
+    except Exception as e:
+        print("Error processing images for section segmentation")
+                                
 
-# #Example usage      
-# image_path = "/Users/lakshita/Desktop/pipeline/yolo/testing/yolo/data/de-skewed-image_94be9b0c81a10c756fcafdc18894383d.jpg"
+
+# image_path = "test2.jpg"
 # img = cv2.imread(image_path)
-# results = yolo_segment(img)
-# print(results)
+# results = yolo_segment(img, 2)
+# print(len(results))
